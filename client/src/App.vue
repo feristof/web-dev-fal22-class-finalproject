8440b6ae24f616a098a7c240b7a9fa2ecf29e08a
Rish <89807042+feristof@users.noreply.github.com>
2022-10-21 11:17:42 -0400

    midterm added

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div id="app">
+  <app-header />
+  <router-view />
+</div>
+</template>
+
+<script lang="ts">
+import AppHeader from "./components/Header.vue"
+
+export default {
+  name: "app",
+  components: {
+    AppHeader,
+  },
+};
+
+</script>
+
+
+
+
+<style scoped>
+
+</style>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+   <span class="icon">
+  <i class="fas fa-home"></i>
+</span>
+</template>
+
+
+
+
+
+
+<script lang="ts">
+export default {};
+</script>
+
+
+<style>
+</style>
